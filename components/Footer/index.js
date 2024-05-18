37cacb4f126d0059f9b92144057c25d752f45d4a
davidcarlino <david.carlino20@gmail.com>
2024-05-18 18:40:08 +1000

    e

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,3 +1,4 @@
+import styles from "../../styles/Pages.module.css";
 import styles from './Footer.module.css';
 
 export default function Footer() {
@@ -20,4 +21,30 @@ export default function Footer() {
       </a>
     </footer>
   );
+}export default function Index({ navigateToPage }) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}></h1>
+        <img
+          src="icons/group-logo.png"
+          alt="Logo"
+          width={270}
+          height={70} />
+
+        <p className={styles.description}>
+        </p>
+
+        <body>
+          <script>
+            function helloWorld()
+          </script>
+
+          <button className={styles.portalButton}
+            type="button" name="portalButton" autofocus onclick="navigate()"><a href="/main-portal" target="_blank">Go To Poortal</a></button>
+        </body>
+      </main>
+    </div>
+  );
 }
+
