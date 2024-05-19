f219d6b959faf88e0cb105a85d3e70ce0c1de6ec
davidcarlino <david.carlino20@gmail.com>
2024-05-19 17:35:02 +1000

    d

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,16 +1,15 @@
-import styles from "../../styles/Pages.module.css";
-import styles1 from './Footer.module.css';
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles1.footer}>
+    <footer className={styles.footer}>
       <a
-        href="https://users.iplatforms.com.au/"
+        href="https://github.com/ibnzUK/next-chrome-starter"
         target="_blank"
         rel="noopener noreferrer"
       >
-        V.1.0.1
-        <span className={styles1.logo}>
+        Footer V.0.0.1
+        <span className={styles.logo}>
           <img
             src="icons/icon16.png"
             alt="Logo"
@@ -22,29 +21,3 @@ export default function Footer() {
     </footer>
   );
 }
-
-export function Index({ navigateToPage }) {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}></h1>
-        <img
-          src="icons/group-logo.png"
-          alt="Logo"
-          width={270}
-          height={70} />
-
-        <p className={styles.description}>
-        </p>
-
-        <body>
-      
-
-          <button className={styles.portalButton}
-            type="button" name="portalButton" autofocus onclick="navigate()"><a href="http://localhost:3000/argon-dashboard-material-ui/dashboard" target="_blank">Go To Portal</a></button>
-        </body>
-      </main>
-    </div>
-  );
-}
-
---
+++ out/next/static/1uWV-RpIpRtmUUYVqHMdJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e93951f2be957ffb.css","static/chunks/pages/index-7e319db1cf9d8ea4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
